8632d8201f3348a5151c9f8c83018901184227c7
Lê Định <93194265+LeDingg@users.noreply.github.com>
2021-11-14 17:19:08 +0700

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -7,4 +7,4 @@
 	commandCategory: "admin",
 	cooldowns: 0
         };
-module.exports.run = ({event, api}) =>api.sendMessage("Địt con mẹ chờ bố mày 1p khởi động lại ... ",event.threadID, () =>process.exit(1))
+module.exports.run = ({event, api}) =>api.sendMessage("Restarting ...",event.threadID, () =>process.exit(1))
